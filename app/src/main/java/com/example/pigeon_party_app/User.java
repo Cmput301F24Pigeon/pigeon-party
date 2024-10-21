ccb0ff2dc077fd60e06d10b775160b0018b99b57
ehambrook <emhambro@ualberta.ca>
2024-10-20 18:56:41 -0600

    US 01.02.01 Entrant information

--- app/src/main/java/com/example/pigeon_party_app/User.java
+++ app/src/main/java/com/example/pigeon_party_app/User.java
@@ -3,36 +3,65 @@ package com.example.pigeon_party_app;
 public class User {
     //first testing with just name
     private String name;
-    //private String email;
-    //private String phoneNumber;             // optional
-    //private boolean entrant;
-    //private boolean organizer;
-    //private boolean admin;
-    // need to make an images for our profile
+    private String email;
+    private String phoneNumber;   // optional
+    private String role;
+//    private boolean entrant;
+//    private boolean organizer;
+//    private boolean admin;
+    //TODO: make images for our profile
 
+    public User(String name, String email, String phoneNumber, String role) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+
+        // Could put (if role == 'entrant') {this.entrant = true;} etc. for bool roles
+    }
 
     public String getName() {
         return name;
     }
 
-   /* public String getEmail() {
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public boolean isEntrant() {
-        return entrant;
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
-    public boolean isOrganizer() {
-        return organizer;
+    public String getRole() {
+        return role;
     }
 
-    public boolean isAdmin() {
-        return admin;
+    public void setRole(String role) {
+        this.role = role;
     }
-     */
+
+//    public boolean isEntrant() {
+//        return entrant;
+//    }
+//
+//    public boolean isOrganizer() {
+//        return organizer;
+//    }
+//
+//    public boolean isAdmin() {
+//        return admin;
+//    }
+
 }

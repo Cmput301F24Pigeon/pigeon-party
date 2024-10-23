030a37a243b293cdcd838d664c6136912843daa4
Chris Cervania <christophercervania@gmail.com>
2024-10-22 22:03:24 -0600

    Fragment for viewing event details

--- app/src/main/java/com/example/pigeon_party_app/EventDetailsFragment.java
+++ app/src/main/java/com/example/pigeon_party_app/EventDetailsFragment.java
@@ -14,7 +14,7 @@ public class EventDetailsFragment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_even_details_fragment);
+        setContentView(R.layout.activity_event_details_fragment);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

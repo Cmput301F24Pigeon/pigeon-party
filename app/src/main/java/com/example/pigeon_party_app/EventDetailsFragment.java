e90740c378704055787934e5c07e48d1d4cd0a58
Chris Cervania <christophercervania@gmail.com>
2024-10-23 15:03:25 -0600

    Display functionality is done

--- app/src/main/java/com/example/pigeon_party_app/EventDetailsFragment.java
+++ app/src/main/java/com/example/pigeon_party_app/EventDetailsFragment.java
@@ -1,6 +1,7 @@
 package com.example.pigeon_party_app;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +9,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
 public class EventDetailsFragment extends AppCompatActivity {
+    private TextView eventName;
+    private TextView eventDateTime;
+    private TextView eventLocation;
+    private TextView eventDetails;
+    private TextView eventCapacity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,16 @@ public class EventDetailsFragment extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    eventName = findViewById(R.id.eventName);
+    eventDateTime = findViewById(R.id.eventDateTime);
+    eventLocation = findViewById(R.id.eventLocation);
+    eventDetails = findViewById(R.id.eventDetails);
+    eventCapacity = findViewById(R.id.eventCapacity);
+
+    eventName.setText(getIntent().getStringExtra("eventName"));
+    eventDateTime.setText("Date and Time:\n" + getIntent().getStringExtra("eventDateTime"));
+    eventLocation.setText("Location:\n" + getIntent().getStringExtra("eventLocation"));
+    eventDetails.setText("Details:\n" + getIntent().getStringExtra("eventDetails"));
+    eventCapacity.setText("Capacity:\n" + getIntent().getStringExtra("eventCapacity"));
     }
 }

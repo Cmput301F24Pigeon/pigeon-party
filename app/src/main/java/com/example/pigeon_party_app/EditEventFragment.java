404e69e9b3b2ecc9ce1c8e6f4f09e11ecd6b66c6
Jax Yopek <jaxyopek@Jaxs-Laptop.local>
2024-11-21 13:47:57 -0700

    added edit event option for organizers

---
+++ app/src/main/java/com/example/pigeon_party_app/EditEventFragment.java
@@ -0,0 +1,241 @@
+package com.example.pigeon_party_app;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link EditEventFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class EditEventFragment extends Fragment {
+    private String eventId;
+    private Calendar selectedDateTime = Calendar.getInstance();
+    private ImageButton dateButton;
+    private TextView eventCreatedMessage;
+    private EditText dateText;
+    private ImageButton uploadImage;
+    private Uri imageUri;
+
+    private final ActivityResultLauncher<PickVisualMediaRequest> pickMediaLauncher =
+            registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), result -> {
+                if (result != null) {
+                    imageUri = result;
+                } else {
+                    Log.e("PickMedia", "No image selected");
+                }
+            });
+
+    public static EditEventFragment newInstance(String eventId) {
+        EditEventFragment fragment = new EditEventFragment();
+        Bundle args = new Bundle();
+        args.putString("eventId", eventId);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            eventId = getArguments().getString("eventId");
+        }
+    }
+
+
+    private DatePickerDialog datePickerDialog;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        View view = inflater.inflate(R.layout.fragment_edit_event, container, false);
+        dateButton = view.findViewById(R.id.button_date_picker);
+        dateText = view.findViewById(R.id.text_event_date);
+
+
+        Button confirmButton = view.findViewById(R.id.button_confirm_edit);
+        ImageButton backButton = view.findViewById(R.id.button_back);
+        EditText editEventTitle = view.findViewById(R.id.edit_event_title);
+        EditText editEventDetails = view.findViewById(R.id.edit_event_details);
+        EditText editWaitlistCap = view.findViewById(R.id.edit_waitlist_cap);
+        Switch editRequiresLocation = view.findViewById(R.id.switch_require_location);
+        uploadImage = view.findViewById(R.id.button_upload_poster);
+        if (eventId != null) {
+            db.collection("events").document(eventId).get()
+                    .addOnSuccessListener(document -> {
+                        if (document.exists()) {
+                            Event event = document.toObject(Event.class);
+                            if (event != null) {
+                                editEventTitle.setText(document.getString("title"));
+                                editEventDetails.setText(document.getString("details"));
+                                editWaitlistCap.setText(String.valueOf(document.getLong("waitlistCapacity")));
+                                editRequiresLocation.setChecked(document.getBoolean("requiresLocation"));
+
+
+                                if (event.getDateTime() != null) {
+                                    selectedDateTime.setTime(event.getDateTime());
+                                    displaySelectedDateTime();
+                                }
+
+
+                                String imageUrl = document.getString("imageUrl");
+                                if (imageUrl != null && !imageUrl.isEmpty()) {
+                                    imageUri = Uri.parse(imageUrl);
+
+                                }
+                            }
+                        }
+                    })
+                    .addOnFailureListener(e -> Log.e("EditEventFragment", "Error fetching event details", e));
+        }
+
+        uploadImage.setOnClickListener(v ->{
+            pickMediaLauncher.launch(new PickVisualMediaRequest.Builder()
+                    .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
+                    .build());
+        });
+        dateButton.setOnClickListener(v -> showDateTimePicker());
+
+
+
+        confirmButton.setOnClickListener(v -> {
+            boolean isValid = true;
+            if (Validator.isEmpty(dateText, "Please select a date and time.")) {
+                isValid = false;
+            }
+            if (Validator.isEmpty(editEventDetails, "Event details cannot be empty")) {
+                isValid = false;
+            }
+            if (Validator.isEmpty(editEventTitle, "Event title cannot be empty")){
+                isValid = false;
+            }
+            if (isValid) {
+                if (editWaitlistCap.getText().toString().isEmpty()){
+                    editWaitlistCap.setText("-1");
+                }
+                Map<String, Object> updatedFields = new HashMap<>();
+                updatedFields.put("title", editEventTitle.getText().toString());
+                updatedFields.put("details", editEventDetails.getText().toString());
+                updatedFields.put("waitlistCapacity", Integer.parseInt(editWaitlistCap.getText().toString()));
+                updatedFields.put("requiresLocation", editRequiresLocation.isChecked());
+                updatedFields.put("dateTime", selectedDateTime.getTime());
+
+                if (imageUri != null) {
+                    updatedFields.put("imageUrl", imageUri.toString());
+                }
+                db.collection("events").document(eventId).update(updatedFields);
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, new OrganizerFragment()) // Change fragment_container to your actual container
+                        .addToBackStack(null)
+                        .commit();
+
+
+            }
+        });
+
+        backButton.setOnClickListener(v -> {
+            getActivity().getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new OrganizerFragment())
+                    .addToBackStack(null)
+                    .commit();
+        });
+
+
+        return view;
+    }
+
+    /**
+     * This is a method to show the date and time to be chosen by the user
+     */
+    private void showDateTimePicker(){
+        showDatePicker();
+    }
+
+    /**
+     * This is a method that formats the selected date and time to be displayed to the user
+     */
+    private void displaySelectedDateTime() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault());
+        String dateString = dateFormat.format(selectedDateTime.getTime());
+        dateText.setText(dateString);
+    }
+
+    /**
+     * This is a method to show the calendar to the user to select the date
+     */
+    private void showDatePicker(){
+        int year = selectedDateTime.get(Calendar.YEAR);
+        int month = selectedDateTime.get(Calendar.MONTH);
+        int day = selectedDateTime.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                getContext(),
+                (view, selectedYear, selectedMonth, selectedDay) -> {
+                    selectedDateTime.set(selectedYear, selectedMonth, selectedDay);
+                    showTimePicker();
+                },
+                year, month, day
+        );
+        datePickerDialog.show();
+    }
+
+    /**
+     * This is a method to show the clock to the user to select the time
+     */
+    private void showTimePicker() {
+        int hour = selectedDateTime.get(Calendar.HOUR_OF_DAY);
+        int minute = selectedDateTime.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(
+                getContext(),
+                (view, selectedHour, selectedMinute) -> {
+                    selectedDateTime.set(Calendar.HOUR_OF_DAY, selectedHour);
+                    selectedDateTime.set(Calendar.MINUTE, selectedMinute);
+                    displaySelectedDateTime();
+                },
+                hour, minute, true
+        );
+        timePickerDialog.show();
+    }
+
+
+
+}

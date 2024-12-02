3d9a6ffbe9cc68100cc271b077df2487eb18339d
Chris Cervania <christophercervania@gmail.com>
2024-12-02 15:37:30 -0700

    test case for eventdetailsfragment

--- app/src/androidTest/java/com/example/pigeon_party_app/EventDetailsFragmentTest.java
+++ app/src/androidTest/java/com/example/pigeon_party_app/EventDetailsFragmentTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import android.util.Log;
+import android.view.View;
 
 import androidx.fragment.app.testing.FragmentScenario;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -40,7 +41,9 @@ public class EventDetailsFragmentTest {
     private String testEventTitle;
     private String testEventDetails;
     private Facility testFacility;
+    private Facility testFacility2;
     private User testUser;
+    private User testUser2;
     private Event testEvent;
 
     /**
@@ -50,76 +53,28 @@ public class EventDetailsFragmentTest {
     public void setUp() {
         db = FirebaseFirestore.getInstance();
         testFacility =  new Facility("test-user-id", "test-address","test-name");
+        testFacility2 =  new Facility("test-user-id2", "test-address2","test-name2");
         testUser = new User("test-user-name","test@email.com",null,"test-user-id",true,true,testFacility,false,"#000000", new ArrayList<String>(), new ArrayList<String>(),false);
+        testUser2 = new User("test-user-name2","test2@email.com",null,"test-user-id2",true,true,testFacility2,false,"#000000", new ArrayList<String>(), new ArrayList<String>(),false);
         Date testDate = new Date();
         Map<String, User> testUsersWaitlist = new HashMap<>();
         Map<String, User> testUsersInvited = new HashMap<>();
         Map<String, User> testUsersCancelled = new HashMap<>();
         testEvent = new Event("test-event-id", "test-title", testDate, 10, "test-details", testFacility, false, testUsersWaitlist, testUsersInvited, testUsersCancelled, testUser);
-
-        db.collection("events").document(testEvent.getEventId())
-                .set(testEvent)
-                .addOnSuccessListener(aVoid -> {
-                    Log.d("FireStore", "Event successfully added");
-                })
-                .addOnFailureListener(e ->{
-                    Log.w("FireStore", "Error adding event", e);
-                });
-        Map<String, Object> updates = new HashMap<>();
-
-        testUser.addOrganizerEventList(testEvent.getEventId());
-        updates.put("organizerEventList", testUser.getOrganizerEventList());
-        db.collection("user").document(testUser.getUniqueId())
-                .update(updates)
-                .addOnSuccessListener(aVoid -> Log.d("Firestore", "User's facility successfully updated"))
-                .addOnFailureListener(e -> Log.w("Firestore", "Error updating user's oranizer list", e));
-
-
     }
 
     /**
-     * Test to check if user can sign up to the event's waitlist
-     *
+     * Test to check if the draw participants button is visible if the user is also the organizer
      */
     @Test
-    public void TestSignUp(){
+    public void TestDrawButtonVisibility(){
+        MainActivity.currentEvent = testEvent;
+        MainActivity.currentUser = testUser;
         FragmentScenario<EventDetailsFragment> scenario = FragmentScenario.launchInContainer(
                 EventDetailsFragment.class
         );
         scenario.onFragment(createdFragment -> {
-            createdFragment.event = testEvent;
-            createdFragment.current_user = testUser;
+            assertEquals(View.VISIBLE, createdFragment.drawParticipantsButton.getVisibility());
         });
-        onView(withId(R.id.signupButton)).perform(click());
-        verifyUserInEventInFirestore(testEvent.getEventId());
-    }
-
-    /**
-     * Method to verify user is in the event's waitlist in firebase
-     * @param eventId
-     */
-    private void verifyUserInEventInFirestore(String eventId) {
-        DocumentReference docRef = db.collection("events").document(eventId);
-        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                if (documentSnapshot.exists()){
-                    assertEquals("User ID should appear as a map key in usersWaitlisted", testUser.getUniqueId(), documentSnapshot.getString("usersWaitlisted."+testUser.getUniqueId()));
-                }
-            }
-        });
-    }
-
-    /**
-     * A method to be performed after the test method to remove any data that was added to the firebase
-     */
-    @After
-    public void removeData() {
-        if (testEventId != null) {
-            db.collection("events").document(testEventId)
-                    .delete()
-                    .addOnSuccessListener(aVoid -> Log.d("Firestore Test", "Test event deleted"))
-                    .addOnFailureListener(e -> Log.w("Firestore Test", "Failed to delete test event", e));
-        }
     }
 }

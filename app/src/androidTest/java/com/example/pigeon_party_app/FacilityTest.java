3871442d4757968f1b3f0fe1693cbae7ed3a990c
ehambrook <emhambro@ualberta.ca>
2024-12-01 18:55:07 -0700

    Add javadocs

--- app/src/androidTest/java/com/example/pigeon_party_app/FacilityTest.java
+++ app/src/androidTest/java/com/example/pigeon_party_app/FacilityTest.java
@@ -10,11 +10,17 @@ public class FacilityTest {
 
     private Facility facility;
 
+    /**
+     * Method to be performed before the test to ensure the necessary attributes are created first
+     */
     @Before
     public void setUp() {
         facility = new Facility("test-user-id", "123 Anywhere Street", "Test Location");
     }
 
+    /**
+     * Tests creating a Facility using the Facility constructor
+     */
     @Test
     public void testFacilityConstructor() {
         assertEquals("test-user-id", facility.getOwnerId());
@@ -22,6 +28,9 @@ public class FacilityTest {
         assertEquals("Test Location", facility.getName());
     }
 
+    /**
+     * Tests Facility class setters and getters
+     */
     @Test
     public void testSettersAndGetters() {
         facility.setName("New Name");
@@ -32,6 +41,9 @@ public class FacilityTest {
         assertEquals("test-user-id", facility.getOwnerId());
     }
 
+    /**
+     * Tests creating a Map from a Facility object
+     */
     @Test
     public void testToMap() {
         Map<String, Object> facilityMap = facility.toMap();
@@ -45,6 +57,9 @@ public class FacilityTest {
         assertEquals("test-user-id", facilityMap.get("ownerId"));
     }
 
+    /**
+     * Tests creating a Map from an empty Facility object
+     */
     @Test
     public void testToMapEmptyValues() {
         Facility emptyFacility = new Facility("", "", "");
